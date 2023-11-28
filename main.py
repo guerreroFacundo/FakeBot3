369a507aa9c402027aa4f83dfff98f489d8c6363
cacoelfacu <39713194+cacoelfacu@users.noreply.github.com>
2023-11-28 02:13:03 +0000

    No se

--- main.py
+++ main.py
@@ -27,39 +27,41 @@ intents = discord.Intents.all()
 intents.message_content = True
 intents.members = True
 #client = discord.Client(intents=intents)
-MY_GUILD = discord.Object(id=1071229378576527492) 
+MY_GUILDS = []
 
 
 
 
 class MyClient(discord.Client):
+  
+    
   def __init__(self, *, intents: discord.Intents):
+      print("Iniciando bot...")
       super().__init__(intents=intents)
-      # A CommandTree is a special type that holds all the application command
-      # state required to make it work. This is a separate class because it
-      # allows all the extra state to be opt-in.
-      # Whenever you want to work with application commands, your tree is used
-      # to store and work with them.
-      # Note: When using commands.Bot instead of discord.Client, the bot will
-      # maintain its own tree instead.
       self.tree = app_commands.CommandTree(self)
-
+  
   # In this basic example, we just synchronize the app commands to one guild.
   # Instead of specifying a guild to every command, we copy over our global commands instead.
   # By doing so, we don't have to wait up to an hour until they are shown to the end-user.
   async def setup_hook(self):
+      
+      print(f"Syncing commands to {len(self.guilds)} guilds...")
       # This copies the global commands over to your guild.
-      self.tree.copy_global_to(guild=MY_GUILD)
-      await self.tree.sync(guild=MY_GUILD)
+      #for guild in MY_GUILDS:
+      # print(f"Syncing commands for {guild.name} ({guild.id})")
+      await self.tree.fetch_commands()
+      await self.tree.sync()
 
 client = MyClient(intents=intents)
 
 @client.event
 async def on_ready():
     print('Loggeado como: {0.user}'.format(client))
-
-
-
+    
+    for guild in client.guilds:
+      print(guild.id)
+      MY_GUILDS.append(guild.id)
+      
 
 
 @client.tree.command()
